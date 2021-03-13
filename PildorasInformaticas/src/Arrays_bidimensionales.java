c5fc8a30e4f9b2ce09e814a29281ee7223cc5bd4
Imorenoa <moreno.igor.87@gmail.com>
2021-03-13 19:13:11 +0100

    Se modifica matriz de arrays bidimensionales

--- PildorasInformaticas/src/Arrays_bidimensionales.java
+++ PildorasInformaticas/src/Arrays_bidimensionales.java
@@ -4,7 +4,7 @@ public class Arrays_bidimensionales {
 	public static void main(String[] args) {
 		
 		int [][] dosDMatrix = {
-				{10,15,12,13,18},
+				{10,15,12,13,23},
 				{5,4,7,58,59},
 				{56,14,1,69,38},
 				{98,87,56,23,45}
