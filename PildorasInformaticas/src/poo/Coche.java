d7f9014b83450fb100f09f309546a8473b15339c
Imorenoa <moreno.igor.87@gmail.com>
2021-03-16 17:37:56 +0100

    Se añaden las clases POO, comenzando

---
+++ PildorasInformaticas/src/poo/Coche.java
@@ -0,0 +1,69 @@
+package poo;
+
+public class Coche {
+
+	/*
+	 * una variable con el modificador de acceso private, es accesible desde la misma clase solo
+	 * 
+	 */
+	
+	private int ruedas;
+	
+	private int largo;
+	
+	private int ancho;
+	
+	private int motor;
+	
+	private int peso_plataforma;
+		
+	private String color;
+	
+	int peso_total;
+	
+	boolean asientos_cuero, climatizador;
+	
+	//Metodo constructor
+
+	public Coche() {
+		
+		ruedas = 4;
+		
+		largo(2000);
+		
+		ancho = 300;
+		
+		motor = 1600;
+		
+		peso_plataforma = 500;
+		
+	}
+	
+	//Metodo getter
+	
+	public String getRuedas() {
+		
+		return "El coche tiene 4 ruedas";
+		
+	}
+	
+	//Metodo setter
+	
+	public void setColor (String color_coche) {
+		
+			color = color_coche;	
+			
+	}
+	public String getColor() {
+		
+		return "El color del coche es " + color;
+		
+	}
+
+	public void setAsientos(String asientos_cuero) {
+		
+		this.asientos_cuero = ;
+		
+	}
+	
+}
---
+++ PildorasInformaticas/src/poo/Uso_coche.java
@@ -0,0 +1,14 @@
+package poo;
+
+public class Uso_coche {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		
+		Coche micoche = new Coche();
+		
+		micoche.setColor("amarillo");
+		
+	}
+
+}
